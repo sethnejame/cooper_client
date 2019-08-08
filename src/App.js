0a53723bb75164679fd6e1fc442834614a299164
Seth NeJame <sethnejame@gmail.com>
2019-08-08 10:49:04 +0200

    Adding app component form fields for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <label>Distance</label>
+          <input id="distance" />
+        </div>
+
+        <select id="gender">
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+
+        <div>
+          <label>Age</label>
+          <input id="age" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
