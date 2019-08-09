39eacb512222a86e43cac5c8627fbdcfc329a8bb
Seth NeJame <sethnejame@gmail.com>
2019-08-09 14:12:20 +0200

    Fixed test to use proper test email

--- cypress/integration/userCanLogin.spec.js
+++ cypress/integration/userCanLogin.spec.js
@@ -3,11 +3,11 @@ describe("User can log in", () => {
     cy.visit("http://localhost:3001");
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@mail.com");
+      cy.get("#email").type("sethnejame@gmail.com");
       cy.get("#password").type("password");
       cy.get("button").click();
     });
-    cy.contains("Hello, user@mail.com!");
+    cy.contains("Hello, sethnejame@gmail.com!");
   });
 
   it("user cant log in with invalid credentials", () => {
